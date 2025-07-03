cb7ef3cd851cc7050d38a1e15cbe1c00200e0043
adrianit248 <adrianit248@gmail.com>
2025-07-03 00:10:35 -0400

    Completed Top Sellers

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,36 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+  // intitialize topSellersList array
+  const [topSellersList, setTopSellersList] = useState([]);
+
+  //initialize load state
+  const [loading, setLoading] = useState(false);
+
+  // define function getNewItemList
+  const getTopSellersList = async () => {
+    setLoading(true);
+    const url =
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers";
+    const response = await axios.get(url);
+    const responseArray = response.data;
+    setTopSellersList(responseArray);
+
+    // mock loading state for 1 second
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+
+  // on mount, use axios to fetch the array data and assign it to the newItemList array
+  useEffect(() => {
+    getTopSellersList();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +43,337 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading ? (
+                <>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="author_list_pp">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "50px", height: "50px", borderRadius: "50%"}}
+                        ></div>
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to='/'>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "100px", height: "20px"}}
+                        ></div>
+                      </Link>
+                      <span>
+                        <div
+                          className="skeleton-box"
+                          style={{width: "40px", height: "20px"}}
+                        ></div>
+                      </span>
+                    </div>
+                  </li>
+                </>
+              ) : (
+                topSellersList.map((elem, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link
+                        to={`/author/${elem.authorId}`}
+                        state={{ elem: elem }}
+                      >
+                        <img
+                          className="lazy pp-author"
+                          src={elem.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link
+                        to={`/author/${elem.authorId}`}
+                        state={{ elem: elem }}
+                      >
+                        {elem.authorName}
+                      </Link>
+                      <span>{elem.price} ETH</span>
+                    </div>
+                  </li>
+                ))
+              )}
             </ol>
           </div>
         </div>
